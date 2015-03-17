fcad24e6f45f36c52383dc90d8fa717989528189
spystation <charles@spystation.com>
2015-03-16 21:43:16 -0400

    Heroku config

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
   root 'pages#welcome'
+
+  get 'pages/portfolio' => 'pages#portfolio'
+
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
